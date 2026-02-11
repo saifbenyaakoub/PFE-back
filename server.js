4a7e23e93c7d45766266fde529980328da3f031e
Saifby <saifbenyaagoub@gmail.com>
2026-02-11 15:21:27 +0100

    services feature

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const serviceRoutes = require("./router/serviceRouter");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Use routes
+app.use("/services", serviceRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
