7ae79d1f7b6b34c5ae1c9402cfdf298c98c1870c
Saifby <saifbenyaagoub@gmail.com>
2026-06-23 11:11:52 +0100

    map update

--- src/models/tasks.js
+++ src/models/tasks.js
@@ -10,9 +10,11 @@ const getAllTasks = async () => {
     t.category,
     t.status,
     t.image_url,
-    u.name           AS client_name,
-    u.id              AS client_id,
-    u.profile_image   AS client_image,
+    u.name             AS client_name,
+    u.id               AS client_id,
+    u.profile_image    AS client_image,
+    u.latitude::float8  AS latitude,
+    u.longitude::float8 AS longitude,
     c.city AS city
   FROM tasks t
   JOIN clients c ON t.client_id = c.id
@@ -32,9 +34,11 @@ const getTaskById = async (id) => {
     t.category,
     t.status,
     t.image_url,
-    u.name           AS client_name,
-    u.id              AS client_id,
-    u.profile_image   AS client_image,
+    u.name             AS client_name,
+    u.id               AS client_id,
+    u.profile_image    AS client_image,
+    u.latitude::float8  AS latitude,
+    u.longitude::float8 AS longitude,
     c.city AS city
   FROM tasks t
   JOIN clients c ON t.client_id = c.id
